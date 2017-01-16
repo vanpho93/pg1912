9abd0109caa40d685368e90ebd49fc14bbc299ec
Admin <vanpho02@gmail.com>
2017-01-16 20:58:08 +0700

    xong

--- db.js
+++ db.js
@@ -27,14 +27,20 @@ var pool = new pg.Pool(config);
 //
 // });
 
-function query(sql){
+function query(sql, cb){
   pool.connect((err, client, done) => {
     if(err) return console.log('Loi: ' + err);
-    console.log('Ket nối thanh cong');
     done();
     client.query(sql,(err, result) => {
       if(err) return console.log('Loi ' + err);
-      console.log(result);
+      //console.log(result.rows);
+      cb(result);
     });
   });
 }
+
+// query('SELECT * FROM "User"',
+// (result) => console.log(result.rows));
+
+query('DELETE FROM "User" WHERE id = 5',
+(result) => console.log(result.rowCount));
